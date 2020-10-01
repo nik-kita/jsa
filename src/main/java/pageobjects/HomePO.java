537e21cabb8d43564f6bb6bac7fed03271c579fd
nik <volsknik@gmail.com>
2020-10-01 16:48:34 +0300

    + public ChallengePO goChallengePage(){...}

--- src/main/java/pageobjects/HomePO.java
+++ src/main/java/pageobjects/HomePO.java
@@ -1,9 +1,19 @@
 package pageobjects;
 
+import locators.GeneralHeaderLocator;
+import locators.HomePageLocator;
 import org.openqa.selenium.WebDriver;
 
 public class HomePO extends BasePageObject {
     public HomePO(WebDriver driver) {
         super(driver);
+    }
+
+    public ChallengePO goChallengePage() {
+        driver.
+                findElement(GeneralHeaderLocator.CHALLENGE_HEADER_BUTTON.getPath()).
+                click();
+
+        return new ChallengePO(driver);
     }
 }
