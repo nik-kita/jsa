172d8dc008c609795b05ece4ea219fb3f64fb0a6
nik <volsknik@gmail.com>
2020-10-01 16:41:05 +0300

    + locators to ChallengePageLocator

--- src/main/java/locators/ChallengePageLocator.java
+++ src/main/java/locators/ChallengePageLocator.java
@@ -6,6 +6,9 @@ public enum ChallengePageLocator implements Locator {
     CREATE_ACCOUNT_BUTTON_INNER_PAGE(By.cssSelector(".inner_page a[href='/users/register/']")),
     EMAIL_INPUT_INNER_PAGE(By.cssSelector("input[name='email']")),
     SIGN_UP_BUTTON_INNER_PAGE(By.cssSelector("button[onclick='submitRegisterInterest()']")),
+    LINK_12_WEEK_CHALLENGE(By.cssSelector("[href='#website-footer']")),
+    PREMIUM_PLAN_LINK(By.cssSelector("[href='/users/pricing/']")),
+
 
 
     ;
