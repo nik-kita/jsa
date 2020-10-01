bce9e1b33e4dd6017a0514963930f40d30bb9d50
nik <volsknik@gmail.com>
2020-10-01 12:52:37 +0300

    CREATE HomePO

---
+++ src/main/java/pageobjects/HomePO.java
@@ -0,0 +1,9 @@
+package pageobjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class HomePO extends BasePageObject {
+    public HomePO(WebDriver driver) {
+        super(driver);
+    }
+}
