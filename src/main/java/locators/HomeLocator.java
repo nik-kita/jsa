02e8be84c0a5f89a4df9afff581ebed3885d5e26
nik <volsknik@gmail.com>
2020-10-01 13:16:05 +0300

    FILL HomeLocator with new locators

--- src/main/java/locators/HomeLocator.java
+++ src/main/java/locators/HomeLocator.java
@@ -4,6 +4,14 @@ import org.openqa.selenium.By;
 
 public enum HomeLocator implements Locator {
     JSA_LOGO(By.cssSelector("a[href='/'] img")),
+    BOOK_LOGO(By.cssSelector(".nav a[href='/not-a-diet-book/'] img")),
+    PRICING_HEADER_BUTTON(By.cssSelector(".nav a[href='/pricing/']")),
+    TRANSFORMATIONS_HEADER_BUTTON(By.cssSelector(".nav a[href='/transformations/']")),
+    CHALLENGE_HEADER_BUTTON(By.cssSelector(".nav a[href='/challenge/about/']")),
+    BLOG_HEADER_BUTTON(By.cssSelector(".nav a[href='/blog/']")),
+    LOGIN_HEADER_BUTTON(By.cssSelector(".nav a[href='/users/login/']")),
+    JOIN_NOW_HEADER_BUTTON(By.cssSelector(".nav a[href='/users/register/']")),
+
 
 
     ;
