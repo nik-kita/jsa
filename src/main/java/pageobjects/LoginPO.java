44c7bc6740e805ad349809991471abfb49121678
nik <volsknik@gmail.com>
2020-10-02 16:48:50 +0300

    FIX LoginPO

--- src/main/java/pageobjects/LoginPO.java
+++ src/main/java/pageobjects/LoginPO.java
@@ -1,9 +1,16 @@
 package pageobjects;
 
 import engine.OnixWebDriver;
+import locators.LoginPageLocator;
 
-public class LoginPO extends BasePageObject {
+public class LoginPO {
+    OnixWebDriver driver;
     public LoginPO(OnixWebDriver driver) {
-        super(driver);
+        this.driver = driver;
+    }
+
+    public HomePO goHomePage() {
+        driver.findElement(LoginPageLocator.JSA_LOGO).click();
+        return new HomePO(driver);
     }
 }
